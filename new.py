c83544d20ef4aec958d02d9aaa93109be031d821
Cristopher Lucas <crislucas.1111@gmail.com>
2024-05-14 07:01:46 -0300

    código atualizado

--- new.py
+++ new.py
@@ -1,41 +1,87 @@
 import os
 
-print('oi')
-
 
 class Modulo():
-    @classmethod
-    def finalizar_app(cls):
+    def finalizar_app(self):
         print('Encerrando o programa\n')
 
-    @classmethod
-    def opcao_invalida(cls, self_):
-        input('Você selecinou uma opção inválida. Digite outra tecla para voltar a menu inicial: ')
+    def voltar_menu(self):
+        input('\nDigite alguma tecla para voltar ao menu inicial: ')
         os.system('cls')
-        App.main(self_)
+        App.main(self)
+
+    def opcao_invalida(self):
+        print('Você selecinou uma opção inválida.')
+        Modulo.voltar_menu(self)
+
+    def cadastrar_restaurante(self, list_):
+        os.system('cls')
+        nome = input('''Cadastro de Restaurantes
+
+Insira o nome do restaurante que deseja cadastrar: ''').title()
+        categoria = input('Digite a categoria do restaurente: ')
+        list_.append({'nome': nome, 'categoria': categoria, 'ativo': False})
+        os.system('cls')
+        print(f'O restaurente {nome} foi adicionado.')
+        Modulo.voltar_menu(self)
+        return list_
+
+    def listar_restaurante(self, list_):
+        print('Listando restaurantes\n\n')
+        print(f'{"Nome do restaurante".ljust(22)} | {"Categoria".ljust(22)}\n')
+        for restaurante in list_:
+            print(
+                f'- {restaurante["nome"].ljust(20)} |\
+{restaurante["categoria"].ljust(20)} | ', end='')
+            print(
+                'O restaurante está ativo' if restaurante['ativo']
+                else 'O restaurante está inativo')
+        Modulo.voltar_menu(self)
+
+    def alterar_estado(self, list_):
+        restaurante_encontrado = False
+        nome = input(
+            'Digite o nome do restaurante que deseja ativar: ').title()
+        for restaurante in list_:
+            if nome == restaurante['nome']:
+                restaurante_encontrado = True
+                restaurante['ativo'] = not restaurante['ativo']
+                os.system('cls')
+                print(
+                    f'O restaurante {nome} foi ativado com sucesso!' if restaurante['ativo']
+                    else f'O restante {nome} foi desativado com sucesso!')
+        if not restaurante_encontrado:
+            print('Restaurante não encontrado.')
+        Modulo.voltar_menu(self)
+        return list_
 
 
 class App(Modulo):
+    def __init__(self):
+        self.lista_restaurante = (
+            {'nome': 'Tavola', 'categoria': 'Pizzaria', 'ativo': False},
+            {'nome': 'Sushiman', 'categoria': 'Japonesa', 'ativo': True},)
+
     def main(self):
         print('''Sabor Express
-        
+
                 1 - Cadastrar restaurante
-                2 - Listar Restaurante
-                3 - Ativar Restaurante
+                2 - Listar restaurante
+                3 - Alterar estado do restaurante
                 4 - Sair  ''')
 
-        opcao_escolhida = int(input('Escolha ma opção: '))
+        opcao_escolhida = int(input('\nEscolha ma opção: '))
         os.system('cls')
         if opcao_escolhida == 1:
-            print('Cadastrar restaurante')
+            super().cadastrar_restaurante(self.lista_restaurante)
         elif opcao_escolhida == 2:
-            print('Listar restaurante')
+            super().listar_restaurante(self.lista_restaurante)
         elif opcao_escolhida == 3:
-            print('Ativar restaurante')
+            super().alterar_estado(self.lista_restaurante)
         elif opcao_escolhida == 4:
-            Modulo.finalizar_app()
+            super().finalizar_app()
         else:
-            Modulo.opcao_invalida(self)
+            super().opcao_invalida()
 
 
 if __name__ == '__main__':
